68721ba80c2ea3d1d5688ccf397fb02aa2d6c070
Fabio Valentini <decathorpe@gmail.com>
2020-10-30 17:50:46 +0100

    refactor manifest functions into methods

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use std::fs;
 use std::io::Write;
 
-use indexmap::IndexMap;
 use structopt::StructOpt;
 
 mod eval;
-use eval::is_linux_target;
-
 mod manifest;
-use manifest::{Dependencies, Dependency, Manifest};
+
+use manifest::Manifest;
 
 #[derive(Debug, StructOpt)]
 #[structopt(setting = structopt::clap::AppSettings::DisableHelpSubcommand)]
@@ -53,91 +51,17 @@ fn write_manifest(path: &str, manifest: &Manifest, preamble: &str) -> Result<(),
 
     let mut file = fs::File::create(path).map_err(|err| format!("Failed to open Cargo.toml: {}", err))?;
 
-    write!(file, "\
+    write!(
+        file,
+        "\
 # This file has been changed by cargoman.
 #
 {}
 
-{}", preamble, string).map_err(|err| format!("Failed to write file: {}", err))?;
-
-    Ok(())
-}
-
-// returned boolean indicates whether the manifest was changed or not
-fn normalize_targets(manifest: &mut Manifest) -> Result<bool, String> {
-    if let Some(ref mut target) = manifest.target {
-        for (key, target) in target.drain(..) {
-            if is_linux_target(&key)? {
-                if let Some(mut dependencies) = target.dependencies {
-                    for (key, dep) in dependencies.drain(..) {
-                        if let Some(ref mut dependencies) = manifest.dependencies {
-                            dependencies.insert(key, dep);
-                        } else {
-                            let mut index: Dependencies = IndexMap::new();
-                            index.insert(key, dep);
-                            manifest.dependencies = Some(index);
-                        }
-                    }
-                }
-
-                if let Some(mut dev_dependencies) = target.dev_dependencies {
-                    for (key, dep) in dev_dependencies.drain(..) {
-                        if let Some(ref mut dev_dependencies) = manifest.dev_dependencies {
-                            dev_dependencies.insert(key, dep);
-                        } else {
-                            let mut index: Dependencies = IndexMap::new();
-                            index.insert(key, dep);
-                            manifest.dev_dependencies = Some(index);
-                        }
-                    }
-                }
-
-                if let Some(mut build_dependencies) = target.build_dependencies {
-                    for (key, dep) in build_dependencies.drain(..) {
-                        if let Some(ref mut build_dependencies) = manifest.build_dependencies {
-                            build_dependencies.insert(key, dep);
-                        } else {
-                            let mut index: Dependencies = IndexMap::new();
-                            index.insert(key, dep);
-                            manifest.build_dependencies = Some(index);
-                        }
-                    }
-                }
-            }
-        }
-
-        manifest.target = None;
-
-        Ok(true)
-    } else {
-        Ok(false)
-    }
-}
-
-fn set_dependency_version(manifest: &mut Manifest, dependency: &str, version: &str) -> Result<(), String> {
-    let doit = |deps: &mut Dependencies| {
-        deps.entry(dependency.to_string()).and_modify(|value| match value {
-            Dependency::Version(ref mut s) => {
-                s.clear();
-                s.push_str(version);
-            },
-            Dependency::Details(ref mut details) => {
-                details.version = Some(version.to_string());
-            },
-        });
-    };
-
-    if let Some(ref mut deps) = manifest.dependencies {
-        doit(deps);
-    }
-
-    if let Some(ref mut deps) = manifest.dev_dependencies {
-        doit(deps);
-    }
-
-    if let Some(ref mut deps) = manifest.build_dependencies {
-        doit(deps);
-    }
+{}",
+        preamble, string
+    )
+    .map_err(|err| format!("Failed to write file: {}", err))?;
 
     Ok(())
 }
@@ -152,7 +76,7 @@ fn main() -> Result<(), String> {
             let (mut manifest, preamble) = read_manifest(path)?;
 
             // only write back to disk if there actually are changes
-            let rewritten = normalize_targets(&mut manifest)?;
+            let rewritten = manifest.normalize_targets()?;
             if rewritten {
                 write_manifest(path, &manifest, &preamble)?;
             }
@@ -165,7 +89,7 @@ fn main() -> Result<(), String> {
             ref version,
         } => {
             let (mut manifest, preamble) = read_manifest(path)?;
-            set_dependency_version(&mut manifest, dependency, version)?;
+            manifest.set_dependency_version(dependency, version)?;
             write_manifest(path, &manifest, &preamble)?;
             Ok(())
         },
--- src/manifest.rs
+++ src/manifest.rs
@@ -2,6 +2,8 @@ use serde::{Deserialize, Serialize};
 
 use indexmap::IndexMap;
 
+use crate::eval::is_linux_target;
+
 // inspired by: https://gitlab.com/crates.rs/cargo_toml
 
 pub type Dependencies = IndexMap<String, Dependency>;
@@ -58,4 +60,85 @@ pub struct DependencyDetails {
     pub package: Option<String>,
     #[serde(flatten)]
     remainder: IndexMap<String, toml::Value>,
+}
+
+impl Manifest {
+    // returned boolean indicates whether the manifest was changed or not
+    pub fn normalize_targets(&mut self) -> Result<bool, String> {
+        if let Some(ref mut target) = self.target {
+            for (key, target) in target.drain(..) {
+                if is_linux_target(&key)? {
+                    if let Some(mut dependencies) = target.dependencies {
+                        for (key, dep) in dependencies.drain(..) {
+                            if let Some(ref mut dependencies) = self.dependencies {
+                                dependencies.insert(key, dep);
+                            } else {
+                                let mut index: Dependencies = IndexMap::new();
+                                index.insert(key, dep);
+                                self.dependencies = Some(index);
+                            }
+                        }
+                    }
+
+                    if let Some(mut dev_dependencies) = target.dev_dependencies {
+                        for (key, dep) in dev_dependencies.drain(..) {
+                            if let Some(ref mut dev_dependencies) = self.dev_dependencies {
+                                dev_dependencies.insert(key, dep);
+                            } else {
+                                let mut index: Dependencies = IndexMap::new();
+                                index.insert(key, dep);
+                                self.dev_dependencies = Some(index);
+                            }
+                        }
+                    }
+
+                    if let Some(mut build_dependencies) = target.build_dependencies {
+                        for (key, dep) in build_dependencies.drain(..) {
+                            if let Some(ref mut build_dependencies) = self.build_dependencies {
+                                build_dependencies.insert(key, dep);
+                            } else {
+                                let mut index: Dependencies = IndexMap::new();
+                                index.insert(key, dep);
+                                self.build_dependencies = Some(index);
+                            }
+                        }
+                    }
+                }
+            }
+
+            self.target = None;
+
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+
+    pub fn set_dependency_version(&mut self, dependency: &str, version: &str) -> Result<(), String> {
+        let doit = |deps: &mut Dependencies| {
+            deps.entry(dependency.to_string()).and_modify(|value| match value {
+                Dependency::Version(ref mut s) => {
+                    s.clear();
+                    s.push_str(version);
+                },
+                Dependency::Details(ref mut details) => {
+                    details.version = Some(version.to_string());
+                },
+            });
+        };
+
+        if let Some(ref mut deps) = self.dependencies {
+            doit(deps);
+        }
+
+        if let Some(ref mut deps) = self.dev_dependencies {
+            doit(deps);
+        }
+
+        if let Some(ref mut deps) = self.build_dependencies {
+            doit(deps);
+        }
+
+        Ok(())
+    }
 }
